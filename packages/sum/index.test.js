667a83d38c0f61ff34785ceee266803f65a7fdaa
Jaka Maver <jm18457@gmail.com>
2025-01-16 11:52:46 +0100

    ci: improve pull request diff (#16)
    * check if JQ exists
    
    * add pull-request
    
    * add pull-request
    
    * add pull-request
    
    * add pull-request
    
    * add pull-request
    
    * add pull-request
    
    * try commits
    
    * add fetch depth
    
    * simplify
    
    * fix
    
    * update for testing

--- packages/sum/index.test.js
+++ packages/sum/index.test.js
@@ -11,3 +11,7 @@ test('sum 2 + 3 to equal 5', () => {
 test('sum 2 + 3 + 4 to equal 9', () => {
   assert.equal(sum(2, 3, 4), 9);
 })
+
+test('sum 2 + 1 to equal 3', () => {
+  assert.equal(sum(2, 1), 3);
+})
