39b67cb87dd2271f68340d1a03c0efe67496a8d5
Jaka Maver <jm18457@gmail.com>
2025-01-16 07:27:58 +0100

    feat: add sum tests

--- packages/sum/index.test.js
+++ packages/sum/index.test.js
@@ -7,3 +7,7 @@ const test = require("node:test");
 test('sum 2 + 3 to equal 5', () => {
   assert.equal(sum(2, 3), 5);
 })
+
+test('sum 2 + 3 + 3 to equal 8', () => {
+  assert.equal(sum(2, 3, 3), 8);
+})
