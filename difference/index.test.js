a90895773e663deae383f4b923aa1688b2c5f2e0
Jaka Maver <jm18457@gmail.com>
2025-01-26 19:27:08 +0100

    feat: remove one test to trigger re-release

--- difference/index.test.js
+++ difference/index.test.js
@@ -4,10 +4,6 @@ const { difference } = require("./index");
 const assert = require("node:assert");
 const test = require("node:test");
 
-test("difference 2 - 2 to equal 0", () => {
-  assert.equal(difference(2, 2), 0);
-});
-
 test("difference 4 - 2 - 1 to equal 1", () => {
   assert.equal(difference(4, 2, 1), 1);
 });
